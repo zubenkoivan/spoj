bed276b16d6c3f921c555bcb958fae5e7852483e
Ivan Zubenko <vanya.zubenko@gmail.com>
2017-07-17 17:29:14 +0300

    prime intervals

--- prime_intervals.py
+++ prime_intervals.py
@@ -6,23 +6,13 @@ def odd_or_next(number, next_delta):
     return number if number & 1 == 1 else (number + next_delta)
 
 
-def set_true(flags, n):
-    mask = 1 << ((n >> 1) & 31)
-    flags[n >> 6] |= mask
-
-
-def is_true(flags, n):
-    mask = 1 << ((n >> 1) & 31)
-    return (flags[n >> 6] & mask) == mask
-
-
 def simple_sieve(limit):
     last_multiple = math.ceil(math.sqrt(limit))
-    is_not_prime = [0 for _ in range((limit >> 6) + 1)]
+    is_not_prime = [False for _ in range(limit)]
     primes = []
 
     for number in range(3, limit + 1, 2):
-        if is_true(is_not_prime, number):
+        if is_not_prime[number]:
             continue
 
         primes.append(number)
@@ -30,16 +20,16 @@ def simple_sieve(limit):
         if number > last_multiple:
             continue
 
-        start = odd_or_next(number * number, number)
-
-        for not_prime in range(start, limit + 1, 2 * number):
-            set_true(is_not_prime, not_prime)
+        for not_prime in range(number * number, limit + 1, 2 * number):
+            is_not_prime[not_prime] = True
 
     return primes
 
 
 def print_interval_primes(start, end, primes, is_not_prime):
-    interval_primes = [2] if start == 2 else []
+    if start == 2:
+        print(2)
+
     start = odd_or_next(start, 1)
     end = odd_or_next(end, -1)
     size = end - start + 1
@@ -50,17 +40,15 @@ def print_interval_primes(start, end, primes, is_not_prime):
         if not_prime > end:
             break
         elif not_prime < start:
-            not_prime = int(math.ceil(start / prime) * prime)
+            not_prime = ((start - 1) // prime + 1) * prime
             not_prime = odd_or_next(not_prime, prime)
 
         for number in range(not_prime - start, size, 2 * prime):
-            set_true(is_not_prime, number)
+            is_not_prime[number] = True
 
-    for number in range(0, size, 2):
-        if not is_true(is_not_prime, number):
-            interval_primes.append(number)
-
-    print('\n'.join(map(str, interval_primes)))
+    print('\n'.join(str(start + number)
+                    for number in range(0, size, 2)
+                    if not is_not_prime[number]))
 
 
 def run():
@@ -68,7 +56,7 @@ def run():
     intervals = [tuple(map(int, input().split())) for _ in range(test_cases)]
     primes = simple_sieve(int(math.sqrt(2147483647)))
     segment_size = int(math.ceil(math.sqrt(2147483647)))
-    segment_buffer = [0 for _ in range((segment_size >> 6) + 1)]
+    segment_buffer = [False for _ in range(segment_size)]
 
     for (start, end) in intervals:
         for segment_start in range(start, end, segment_size):
