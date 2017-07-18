9d983111b03e21a3805e486a6569b9f2bd96bf96
Ivan Zubenko <vanya.zubenko@gmail.com>
2017-07-19 00:50:23 +0300

    palindrome

--- the_next_palindrome.py
+++ the_next_palindrome.py
@@ -1,10 +1,63 @@
+def right_gte_left(integer):
+    right_start = len(integer) // 2
+    for i in range(right_start, len(integer)):
+        left = int(integer[len(integer) - i - 1])
+        right = int(integer[i])
+        if right > left:
+            return True
+        elif right < left:
+            return False
+    return True
+
+
+def mirror_left(integer):
+    middle = (len(integer) - 1) // 2 + 1
+    left = integer[0:middle]
+    if len(integer) & 1 == 0:
+        return left + left[::-1]
+    return left + left[-2::-1]
+
+
+def increment_left(integer):
+    middle = (len(integer) - 1) // 2
+    left = list(integer[middle::-1])
+    carry = 1
+    for i in range(len(left)):
+        if left[i] == '9':
+            left[i] = '0'
+        else:
+            left[i] = str(int(left[i]) + 1)
+            carry = 0
+            break
+    return (carry, left)
+
+def mirror_incremented_left(integer):
+    carry, left = increment_left(integer)
+    if carry == 1:
+        left.append('1')
+        if len(integer) & 1 == 0:
+            left_str = ''.join(left)
+            return left_str[:0:-1] + left_str
+        else:
+            left_str = ''.join(left[1:])
+            return left_str[::-1] + left_str
+    left_str = ''.join(left)
+    if len(integer) & 1 == 0:
+        return left_str[::-1] + left_str
+    return left_str[:0:-1] + left_str
+
+
 def next_palindrome(integer):
-    pass
+    integer = integer if len(integer) == 1 else integer.lstrip('0')
+    if right_gte_left(integer):
+        return mirror_incremented_left(integer)
+    return mirror_left(integer)
 
 
 def run():
     test_cases = int(input())
-    palindromes = '\n'.join(next_palindrome(input()) for _ in range(test_cases))
+    palindromes = '\n'.join(next_palindrome(input())
+                            for _ in range(test_cases))
     print(palindromes)
 
 
