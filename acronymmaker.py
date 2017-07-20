e2b265ca1ef60be2ba9322d6aeba0493b5f59bf2
Ivan Zubenko <vanya.zubenko@gmail.com>
2017-07-20 17:02:05 +0300

    acronymmaker

--- acronymmaker.py
+++ acronymmaker.py
@@ -3,35 +3,53 @@ import itertools
 
 def acronym_count(insign_words, line):
     acronym, words = split_words(insign_words, line)
+    max_symbols_per_word = len(acronym) - len(words) + 1
+    if max_symbols_per_word < 1:
+        return (acronym.upper(), 0)
     acc_words_lens = get_acc_words_lens(words)
-    prefix_counts = [[0 for _ in range(len(acronym) + 1)]
-                     for _ in range(len(words) + 1)]
+    prefix_counts = [[0] * (len(acronym) + 1) for _ in range(len(words) + 1)]
+    prefix_counts[0][0] = 1
     words = ''.join(words)
     test_case = (prefix_counts, acc_words_lens, acronym, words)
-    fill_prefix_counts(test_case, (1, 0, 0, 0))
-    return prefix_counts[-1][-1]
+    for word in range(1, len(prefix_counts)):
+        for acronym_idx in range(word - 1, word - 1 + max_symbols_per_word):
+            fill_prefix_counts(test_case, (word, acc_words_lens[word - 1], 0, acronym_idx))
+    return (acronym.upper(), prefix_counts[-1][-1])
 
 
 def fill_prefix_counts(test_case, state):
     prefix_counts, acc_words_lens, acronym, words = test_case
-    word, words_idx, word_acronym_len, acronym_idx = state
+    word, words_idx, word_matched_len, acronym_idx = state
+    if words_idx == acc_words_lens[word] or acronym_idx == len(acronym):
+        return
+    max_acronym_idx = len(acronym) - (len(prefix_counts) - 1 - word)
     for i in range(words_idx, acc_words_lens[word]):
         if words[i] == acronym[acronym_idx]:
-            fork_state = (word, i + 1, word_acronym_len, acronym_idx)
+            count = prefix_counts[word - 1][acronym_idx - word_matched_len]
+            prefix_counts[word][acronym_idx + 1] += count
+
+            fork_state = (word, i + 1, word_matched_len, acronym_idx)
             fill_prefix_counts(test_case, fork_state)
+
+            word_matched_len += 1
+            acronym_idx += 1
+
+            if acronym_idx == max_acronym_idx:
+                break
 
 
 def split_words(insign_words, line):
     words = line.split()
     acronym = words[0].lower()
     words = itertools.islice(words, 1, len(words))
-    words = [filter(lambda x: insign_words[x] is not None, words)]
+    words = list(filter(lambda x: insign_words.get(x) is None, words))
     return (acronym, words)
 
 
 def get_acc_words_lens(words):
-    lens = [0].extend(map(len, words))
-    for i in range(1, len(words)):
+    lens = [0]
+    lens.extend(map(len, words))
+    for i in range(1, len(lens)):
         lens[i] += lens[i - 1]
     return lens
 
